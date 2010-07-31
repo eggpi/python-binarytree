1a60aa6fea6122c641b38a1de09c6809e7256f6c
Guilherme Gonçalves <guilherme@las.ic.unicamp.br>
2010-07-31 11:54:03 -0300

    Removed garbage collection support for now. Initial implementation of insertion.
    Something was broken in garbage collection, causing seemingly random
    segmentation faults.

--- binarytree.c
+++ binarytree.c
@@ -45,12 +45,20 @@ typedef struct {
 
 /* Prototypes for NodeType methods */
 static void Node_dealloc(Node * self);
+static Node * Node_insert(Node * root, Node * new);
+
+/* XXX - Needed for garbage collection, which I haven't managed to get to work
 static int Node_traverse(Node * self, visitproc visit, void * arg);
+*/
 static void Node_clear(Node * self);
 
 /* Prototypes for BinaryTreeType methods */
 static void BinaryTree_dealloc(BinaryTree * self);
+static PyObject * BinaryTree_insert(BinaryTree * self, PyObject * new);
+
+/* XXX - Needed for garbage collection, which I haven't managed to get working
 static int BinaryTree_traverse(BinaryTree * self, visitproc visit, void * arg);
+*/
 static void BinaryTree_clear(BinaryTree * self);
 
 /* Left and right rotation */
@@ -65,6 +73,13 @@ static PyTypeObject BinaryTreeType = {
 	PyObject_HEAD_INIT(NULL)
 };
 
+static PyMethodDef BinaryTree_methods[] = {
+	{ "insert", (PyCFunction) BinaryTree_insert, METH_O,
+	"Inserts the parameter into the tree, without creating duplicates"
+	},
+	{NULL}, /* Sentinel */
+};
+
 static void Node_dealloc(Node * self) {
 	Node_clear(self);
 	Py_TYPE((PyObject *) self)->tp_free((PyObject *) self);
@@ -72,6 +87,7 @@ static void Node_dealloc(Node * self) {
 	return;
 }
 
+/*
 static int Node_traverse(Node * self, visitproc visit, void * arg) {
 	Py_VISIT(self->item);
 	Py_VISIT((PyObject *) self->lchild);
@@ -79,6 +95,7 @@ static int Node_traverse(Node * self, visitproc visit, void * arg) {
 
 	return 0;
 }
+*/
 
 static void Node_clear(Node * self) {
 	Py_CLEAR(self->item);
@@ -95,11 +112,12 @@ static void BinaryTree_dealloc(BinaryTree * self) {
 	return;
 }
 
+/*
 static int BinaryTree_traverse(BinaryTree * self, visitproc visit, void * arg) {
 	Py_VISIT(self->root);
 
 	return 0;
-}
+}*/
 
 static void BinaryTree_clear(BinaryTree * self) {
 	Py_CLEAR(self->root);
@@ -144,6 +162,121 @@ static Node * rotateRight(Node * root) {
 	return root;
 }
 
+static Node * Node_insert(Node * root, Node * new) {
+	int left_right; /* -1 when descending left, 1 for right */
+	int child_balance;
+	Node * newchild;
+
+	left_right = PyObject_Compare(root->item, new->item);
+
+	switch ( left_right ) {
+		case 0:
+			return root; /* Node already in the tree */
+		case -1:
+			if ( root->lchild == NULL ) {
+				/* Base case: simple insertion */
+				Py_INCREF(new);
+				root->lchild = new;
+				root->balance--;
+
+				assert(root->balance <= 1);
+				assert(root->balance >= -1);
+				return root;
+			}
+
+			/* Descend left */
+			child_balance = root->lchild->balance;
+			newchild = Node_insert(root->lchild, new);
+
+			if ( newchild != root->lchild ) {
+				/* Tree has already been rotated */
+				root->lchild = newchild;
+				return root;
+			}
+
+			root->balance--;
+			assert(root->balance <= 1);
+			if ( root->balance > -2 ) return root;
+
+			if ( child_balance < root->lchild->balance ) {
+				/* Left-right case */
+				root->lchild = rotateLeft(root->lchild);
+			}
+
+			/* Left-left case */
+			return rotateRight(root);
+
+		case 1:
+			if ( root->rchild == NULL ) {
+				Py_INCREF(new);
+				root->rchild = new;
+				root->balance++;
+				
+				assert(root->balance <= 1);
+				assert(root->balance >= -1);
+			}
+
+			/* Descend right */
+			child_balance = root->rchild->balance;
+			newchild = Node_insert(root->rchild, new);
+
+			if ( newchild != root->rchild ) {
+				root->rchild = newchild;
+				return root;
+			}
+
+			root->balance++;
+			assert(root->balance >= -1 );
+			if ( root->balance < 2 ) return root;
+
+			if ( child_balance > root->rchild->balance ) {
+				/* Right-left case */
+				root->rchild = rotateRight(root->rchild);
+			}
+
+			/* Right-right case */
+			return rotateLeft(root);
+
+		default:
+			return NULL; /* Unexpected result in comparison */
+	}
+}
+
+/* Inserts 'new' into a binary tree.
+ * Returns 1 on success, 0 on error. */
+static PyObject * BinaryTree_insert(BinaryTree * self, PyObject * new) {
+	Node * newnode, * newroot = NULL;
+
+	/* Create a new container */
+	newnode = PyObject_New(Node, &NodeType);	
+	if ( newnode == NULL ) return NULL;
+
+	newnode->lchild = NULL;
+	newnode->rchild = NULL;
+	newnode->balance = 0;
+
+	Py_INCREF(new);
+	newnode->item = new;
+
+	if ( self->root != NULL ) {
+		/* Insert into the existing tree */
+		newroot = Node_insert(self->root, newnode);
+		
+		if ( newroot == NULL ) {
+			Py_DECREF(newnode);
+			return NULL;
+		}
+		
+		self->root = newroot;
+	} else {
+		/* First insertion */
+		self->root = newnode;
+	}
+
+	self->ob_size++;
+	Py_RETURN_NONE;
+}
+
 #ifndef PyMODINIT_FUNC
 #define PyMODINIT_FUNC void
 #endif
@@ -152,13 +285,12 @@ initbinarytree(void) {
 	PyObject * module;
 
 	/* NodeType setup */
-	NodeType.tp_new = PyType_GenericNew;
+	NodeType.tp_new = (newfunc) PyType_GenericNew;
 	NodeType.tp_basicsize = sizeof(Node);
 	NodeType.tp_name = "binarytree.Node";
 	NodeType.tp_doc = "An internal data container.";
-	NodeType.tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_GC;
-	NodeType.tp_traverse = (traverseproc) Node_traverse;
-	NodeType.tp_clear = (inquiry) Node_clear;
+	NodeType.tp_flags = Py_TPFLAGS_DEFAULT;
+	NodeType.tp_alloc = PyType_GenericAlloc;
 	NodeType.tp_dealloc = (destructor) Node_dealloc;
 	
 	if ( PyType_Ready(&NodeType) < 0 ) return;
@@ -169,11 +301,9 @@ initbinarytree(void) {
 	BinaryTreeType.tp_name = "binarytree.BinaryTree";
 	BinaryTreeType.tp_doc = "The main binary tree class.";
 	BinaryTreeType.tp_flags = Py_TPFLAGS_DEFAULT |
-				Py_TPFLAGS_BASETYPE |
-				Py_TPFLAGS_HAVE_GC;
-	BinaryTreeType.tp_traverse = (traverseproc) BinaryTree_traverse;
-	BinaryTreeType.tp_clear = (inquiry) BinaryTree_clear;
+				Py_TPFLAGS_BASETYPE;
 	BinaryTreeType.tp_dealloc = (destructor) BinaryTree_dealloc;
+	BinaryTreeType.tp_methods = BinaryTree_methods;
 
 	if ( PyType_Ready(&BinaryTreeType) < 0 ) return;
 
