27ecf6809598c3357f0d9491fe671ed4cec1e55e
Guilherme Gonçalves <guilherme@las.ic.unicamp.br>
2011-03-08 12:29:24 -0300

    Updated README and copyright notes.

--- binarytree.c
+++ binarytree.c
@@ -1,4 +1,4 @@
-/*  Copyright 2010, Guilherme Gonçalves (guilherme.p.gonc@gmail.com)
+/*  Copyright 2010, 2011 Guilherme Gonçalves (guilherme.p.gonc@gmail.com)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
